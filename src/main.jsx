08771750351d2bf3909445c38fe2b653a4920525
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2023-03-01 11:52:34 -0800

    3rd, child routes and forms covered

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Root, { loader as rootLoader } from './routes/root';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root';
 import ErrorPage from './error-page';
-import Contact from './routes/contact';
+import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact from './routes/edit';
 import './index.css'
 
 const router = createBrowserRouter([
@@ -13,10 +14,17 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
       },
     ],
   },
---
+++ src/routes/edit.jsx
@@ -0,0 +1,31 @@
+import { Form, useLoaderData } from 'react-router-dom';
+
+export default function EditContact() {
+  const contact = useLoaderData();
+
+  return (
+    <Form method ="POST" id="contact-form">
+      <p>
+        <span>Name</span>
+        <input type="text" name="first" placeholder="First" aria-label="First name" defaultValue={contact.first} />
+        <input type="text" name="last" placeholder="Last" aria-label="Last name" defaultValue={contact.last} />
+      </p>
+      <label>
+        <span>Twitter</span>
+        <input type="text" name="twitter" placeholder="@jack" defaultValue={contact.twitter} />
+      </label>
+      <label>
+        <span>Avatar URL</span>
+        <input type="text" placeholder="https://example.com/avatar.jpg" aria-label="Avatar URL" name="avatar" defaultValue={contact.avatar} />
+      </label>
+      <label>
+        <span>Notes</span>
+        <textarea name="notes" rows="6" defaultValue={contact.notes} />
+      </label>
+      <p>
+        <button type="submit">Save</button>
+        <button type="button">Cancel</button>
+      </p>
+    </Form>
+  );
+}
